d6bb0e3d4962f335e9048fe7eed817ea971ab48a
AOI <miho0_0@naver.com>
2025-05-25 20:41:21 +0900

    preset loader for fox2av program

---
+++ presets.py
@@ -0,0 +1,111 @@
+import os
+from datetime import datetime
+import configparser
+
+# 폴더 경로
+logs_dir = os.path.abspath("./Common/Logs")
+settings_dir = os.path.abspath("./Common/Settings")
+quarantine_dir = os.path.abspath("./Common/Quarantine")
+
+# 설정 파일 경로
+settings_file_path = os.path.join(settings_dir, "fox2av_settings.ini")
+
+
+def fox2av_preset_maker():
+    # 폴더 생성
+    os.makedirs(logs_dir, exist_ok=True)
+    os.makedirs(settings_dir, exist_ok=True)
+    os.makedirs(quarantine_dir, exist_ok=True)
+
+def recent_scanDate_maker():
+    print("fox2av-preset -- recent_scanDate_maker tester")
+
+def fox2av_startup_set_manager():
+    print("fox2av-preset -- fox2av_startup_set_manager tester")
+    recent_scanDate_maker()
+
+"""
+프로그램 초기 설정 ini 파일임.
+만약 사용자가 설정 초기화 버튼을 눌렀다면, ini 파일 삭제하고, 이 함수 실행하면 됨
+"""
+def reRoll_to_default_set():
+    # 설정 파일 생성 및 작성
+    config = configparser.ConfigParser()
+
+    # 설정 파일의 기본 섹션 및 설정 값
+
+    # 1. General Settings
+    config['General'] = {
+        'real_time_protection': 'off',
+        'protection_mode': 'normal',
+        'auto_update': 'disabled',
+        'update_interval': '24'  # 시간 단위
+    }
+
+    # 2. Network Settings
+    config['Network'] = {
+        'use_proxy': 'false',
+        'proxy_address': '192.168.1.100',
+        'proxy_port': '8080',
+        'firewall_enabled': 'false',
+        'allow_inbound_connections': 'true'
+    }
+
+    # 3. Scan Settings
+    config['Scan'] = {
+        'auto_virus_scan': 'off',
+        'scan_time': '03:00',
+        'scan_type': 'full',
+        'exclusion_paths': 'C:\\Program Files\\MyApp, D:\\Data',
+        'exclusion_extensions': '.tmp, .log'
+    }
+
+    # 4. Notification Settings
+    config['Notifications'] = {
+        'show_notifications': 'true',
+        'notification_sound': 'true',
+        'critical_alerts_only': 'false',
+        'log_path': 'C:\\Program Files\\Fox2av\\Logs\\',
+        'log_level': 'verbose'
+    }
+
+    # 5. User Information
+    config['User'] = {
+        'license_key': 'ABCD-1234-EFGH-5678',
+        'user_id': 'user1234',
+        'registered': 'true'
+    }
+
+    # 6. Update Settings
+    config['Update'] = {
+        'last_update_dbVer': '0.0.2',
+        'last_update_date': '2024-09-23',
+        'latest_version': '1.0.23',
+        'update_server': 'update.antivirus.com',
+        'fallback_server': 'backup.antivirus.com'
+    }
+
+    # 7. Quarantine Information
+    config['Quarantine'] = {
+        'quarantine_path': 'C:\\Program Files\\Fox2av\\Quarantine\\',
+        'quarantine_retention_days': '30'
+    }
+
+    # 8. Scan History
+    config['ScanHistory'] = {
+        'last_scan_date': '2024-09-23 10:34:11',
+        'last_scan_result': 'no threats found',
+        'last_scan_duration': '00:15:23'
+    }
+
+    # 9. Exclusion Settings
+    config['Exclusions'] = {
+        'file_exclusions': 'C:\\Program Files\\SafeApp\\safeapp.exe',
+        'folder_exclusions': 'D:\\Backup',
+        'extension_exclusions': '.iso, .bak'
+    }
+
+    # 설정 파일이 없는 경우에만 생성
+    if not os.path.exists(settings_file_path):
+        with open(settings_file_path, 'w') as configfile:
+            config.write(configfile)
